015254d064a156e75caa2c780cfcd343bba04f8f
Dhruvdutt Jadhav <jdhruv12@gmail.com>
2015-11-18 00:14:05 +0530

    Initial

---
+++ resolutions.js
@@ -0,0 +1,51 @@
+Resolutions = new Mongo.Collection('resolutions');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    resolutions: function() {
+      if(Session.get('hideFinished')) {
+        return Resolutions.find({checked: {$ne: true}});
+      }
+      else {
+        return Resolutions.find();  
+      }
+    },
+    hideFinished: function() {
+      return Session.get('hideFinished');
+    }
+  });
+
+  Template.body.events({
+    'submit .new-resolution' : function(event) {
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        title: title,
+        createdAt: new Date()
+      });
+
+      event.target.title.value = "";
+      return false;
+    },
+
+    'change .hide-finish': function(event) {
+      Session.set('hideFinished', event.target.checked);
+    }
+  });
+
+  Template.resolution.events({
+    'click .toggle-checked': function() {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}});
+    },
+
+    'click .delete': function() {
+      Resolutions.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
