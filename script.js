77858be134049ead7492597dfd8421ea7d14ade6
Dhruvdutt Jadhav <jdhruv12@gmail.com>
2015-11-18 15:05:43 +0530

    Improved File Structure

--- script.js
+++ script.js
@@ -39,34 +39,6 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.resolution.helpers({
-    isOwner: function() {
-      return this.owner === Meteor.userId();
-    }
-  });
-
-  Template.resolution.events({
-    'click .toggle-checked': function() {
-
-      Meteor.call("updateResolution", this._id, !this.checked);
-      /* Insecure Way
-      Resolutions.update(this._id, {$set: {checked: !this.checked}});
-      */
-    },
-
-    'click .delete': function() {
-
-      Meteor.call("deleteResolution", this._id);
-      /* Insecure Way
-      Resolutions.remove(this._id);
-      */
-    },
-
-    'click .toggle-private': function() {
-      Meteor.call("setPrivate", this._id, !this.private);
-    }
-  });
-
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
